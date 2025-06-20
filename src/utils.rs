6cd75281914276938210f466afa6974547d3e096
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-06-20 13:13:21 +0100

    optimize imported func count

--- src/utils.rs
+++ src/utils.rs
@@ -6,9 +6,6 @@ use parity_wasm::elements;
 
 /// Gather all the imported function ids.
 pub fn imported_function_ids(module: &elements::Module) -> BTreeSet<u32> {
-	let func_imports = module.import_count(elements::ImportCountType::Function);
-
-	(0..module.functions_space())
-		.filter_map(|func_idx| if func_idx < func_imports { Some(func_idx as u32) } else { None })
-		.collect()
+	let func_imports = module.import_count(elements::ImportCountType::Function) as u32;
+	(0..func_imports).collect()
 }
